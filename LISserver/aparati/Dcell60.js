3cfccc384f20a67aea8f8200c69706fd3cbe9c81
ATOM | lis <atom.bitbucket@gmail.com>
2019-10-05 14:33:17 +0200

    05.10.2019

--- LISserver/aparati/Dcell60.js
+++ LISserver/aparati/Dcell60.js
@@ -118,6 +118,41 @@ module.exports = {
                                         analit_rez:element.substring(76,79)+'.'+element.substring(79,80),
                                         analit_status:''
                                     }) 
+                                    rezultati.push({
+                                        analit:'RDW-CV',
+                                        analit_rez:element.substring(80,82)+'.'+element.substring(82,83),
+                                        analit_status:''
+                                    }) 
+                                    rezultati.push({
+                                        analit:'HCT',
+                                        analit_rez:element.substring(83,85)+'.'+element.substring(85,86),
+                                        analit_status:''
+                                    })
+                                    rezultati.push({
+                                        analit:'PLT',
+                                        analit_rez:element.substring(86,90),
+                                        analit_status:''
+                                    })  
+                                    rezultati.push({
+                                        analit:'MPV',
+                                        analit_rez:element.substring(90,92)+'.'+element.substring(92,93),
+                                        analit_status:''
+                                    }) 
+                                    rezultati.push({
+                                        analit:'PDW',
+                                        analit_rez:element.substring(93,95)+'.'+element.substring(95,96),
+                                        analit_status:''
+                                    }) 
+                                    rezultati.push({
+                                        analit:'PCT',
+                                        analit_rez:'0.'+element.substring(96,99),
+                                        analit_status:''
+                                    }) 
+                                    rezultati.push({
+                                        analit:'RDW-SD',
+                                        analit_rez:element.substring(99,102)+'.'+element.substring(102,103),
+                                        analit_status:''
+                                    }) 
                                     console.log(rezultati)
                                     break;
                           case 'C':
