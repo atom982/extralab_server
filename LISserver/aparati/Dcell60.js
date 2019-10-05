92a8c633667c15c8813ff0832d31a4f866328924
ATOM | lis <atom.bitbucket@gmail.com>
2019-10-05 14:52:09 +0200

    05.10.2019

--- LISserver/aparati/Dcell60.js
+++ LISserver/aparati/Dcell60.js
@@ -118,11 +118,11 @@ module.exports = {
                                         analit_rez:element.substring(76,79)+'.'+element.substring(79,80),
                                         analit_status:''
                                     }) 
-                                    // rezultati.push({
-                                    //     analit:'RDW-CV',
-                                    //     analit_rez:element.substring(80,82)+'.'+element.substring(82,83),
-                                    //     analit_status:''
-                                    // }) 
+                                    rezultati.push({
+                                        analit:'RDW',
+                                        analit_rez:element.substring(80,82)+'.'+element.substring(82,83),
+                                        analit_status:''
+                                    }) 
                                     rezultati.push({
                                         analit:'HCT',
                                         analit_rez:element.substring(83,85)+'.'+element.substring(85,86),
