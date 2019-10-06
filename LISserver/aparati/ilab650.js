e66c3d921e95bb779a0b88b80c3902a14b8bae07
ATOM | lis <atom.bitbucket@gmail.com>
2019-10-06 13:25:08 +0200

    06.10.2019

--- LISserver/aparati/ilab650.js
+++ LISserver/aparati/ilab650.js
@@ -498,8 +498,8 @@ module.exports = {
                   }else{
                     nrTests = nrTests.toString()
                   }
-                  //                                                                                                              scpos  stype   sex   cuptype    dil   rerun reflex   doctor   nrTests   (testtype+testno)
-                  var order ='\u0002'+ 'W10000'+uzorak.id+'    '+'0'+uzorak.id+"      "+'191006'+'1130'+'1'+'50'+   '1'+   '1'+  '1' +     '001'+ '0'+    '0'+   '001'+   '1'+      '1'   + '002'+'\u0003'
+                  //                                                                  yymmdd       time     diskno    scpos   stype   sex   cuptype    dil   rerun reflex   doctor   nrTests   (testtype+testno)
+                  var order ='\u0002'+ 'W10000'+uzorak.id+'  '+'0'+uzorak.id+"      "+'191006'+ '1130'+   '1'      +'50'+   '1'+   '1'+  '1' +     '001'+ '0'+    '0'+   '001'+   '001'+      '1'   + '002'+'\u0003'
                   // order +=nrTests
                   // order +=allTST
                   //   allTST = ''
