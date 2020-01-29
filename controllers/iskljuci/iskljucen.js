ddf1b1fd1e2a125a0a83931ca358cedb7db1349d
ATOM | Lab Software <atom.bitbucket@gmail.com>
2020-01-29 12:14:35 +0100

    29.01.2020 -6

--- controllers/iskljuci/iskljucen.js
+++ controllers/iskljuci/iskljucen.js
@@ -1,7 +1,7 @@
 module.exports = {
   test: function(test, site) {
     // console.log(test)
-    if ((test === "HGB - KKS3" || test === "HbA1c - ILab 650")) {
+    if ((test === "HGB - KKS3" || test === "HbA1c - ILab 650" || test === "HDL - ILab 650")) {
       return false;
     } else {
       return true;
