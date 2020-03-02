a065a81355266ae4e52d3ad79f4f988f74e688eb
ATOM | lis <atom.bitbucket@gmail.com>
2020-03-02 23:50:29 +0100

    02-mar-2020

--- LISserver/server.js
+++ LISserver/server.js
@@ -104,7 +104,6 @@ class lisServer {
             console.log('HL7 data received:')
             HL7data = HL7data.substring(HL7data.indexOf("\u000b") + 1, HL7data.indexOf("\u001c") - 1)  
             funkcija.parsaj_hl7(HL7data, function (poruka) {
-              Order_Response+"\u000f"+Order_Download
               var orders = poruka.split("\u000f")
               lisserver.broadcast(orders[0], client)
               lisserver.broadcast(orders[1], client)
