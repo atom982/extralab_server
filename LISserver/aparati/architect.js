fc70f19d8ef4ba56fb51d2560a235e35629d7fa7
ATOM | lis <atom.bitbucket@gmail.com>
2020-09-10 23:43:42 +0200

    10-09-2020

--- LISserver/aparati/architect.js
+++ LISserver/aparati/architect.js
@@ -447,7 +447,7 @@ module.exports = {
     });
   },
 
-  parsaj_query: function (record, callback) {
+  parsaj_query: function (record,serijski, callback) {
 
     var mongoose = require("mongoose");
     var Samples = require("../../models/Postavke");
